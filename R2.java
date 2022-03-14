cd727811ad9ee585d4c3f61ab9f1da58935bdf2c
Роман Гірковий <99317452+Ch1ZaZ@users.noreply.github.com>
2022-03-14 12:40:33 +0200

    Roman Gera

---
+++ R2.java
@@ -0,0 +1,7 @@
+package  com.company;
+public class R2 {
+    public static void main(String[] args) {
+        int c = 56+44
+        System.out.println("Сума двохзначних чисел= "+ c);
+    }
+}
---
+++ R3.java
@@ -0,0 +1,8 @@
+package  com.company;
+public class R3 {
+    public static void main(String[] args){
+        double e = 3.3
+        System.out.println(Math.round(+e));
+
+    }
+}
